3426ff939b5c5adb6f5fef06cf308e1e36c0c94f
Jonathan S <gereeter@gmail.com>
2014-01-02 12:23:23 -0600

    Change to specialized Word/Int conversion functions

--- src/Data/IntMap/Bounded.hs
+++ src/Data/IntMap/Bounded.hs
@@ -88,6 +88,8 @@ import Data.Bits (xor)
 import Data.WordMap.Internal (WordMap(..), Node(..))
 import qualified Data.WordMap as W
 
+import Data.Word (Word)
+
 import Prelude hiding (lookup, null, map, filter, foldl, foldr, min, max)
 
 type Key = Int
@@ -130,48 +132,48 @@ size :: IntMap a -> Int
 size (IntMap m) = W.size m
 
 member :: Key -> IntMap a -> Bool
-member k (IntMap m) = W.member (fromIntegral k) m
+member k (IntMap m) = W.member (i2w k) m
 
 notMember :: Key -> IntMap a -> Bool
-notMember k (IntMap m) = W.notMember (fromIntegral k) m
+notMember k (IntMap m) = W.notMember (i2w k) m
 
 lookup :: Key -> IntMap a -> Maybe a
-lookup k (IntMap m) = W.lookup (fromIntegral k) m
+lookup k (IntMap m) = W.lookup (i2w k) m
 
 findWithDefault :: a -> Key -> IntMap a -> a
-findWithDefault def k (IntMap m) = W.findWithDefault def (fromIntegral k) m
+findWithDefault def k (IntMap m) = W.findWithDefault def (i2w k) m
 
 empty :: IntMap a
 empty = IntMap W.empty
 
 singleton :: Key -> a -> IntMap a
-singleton k v = IntMap (W.singleton (fromIntegral k) v)
+singleton k v = IntMap (W.singleton (i2w k) v)
 
 insert :: Key -> a -> IntMap a -> IntMap a
-insert k v (IntMap m) = IntMap (W.insert (fromIntegral k) v m)
+insert k v (IntMap m) = IntMap (W.insert (i2w k) v m)
 
 insertWith :: (a -> a -> a) -> Key -> a -> IntMap a -> IntMap a
-insertWith f k v (IntMap m) = IntMap (W.insertWith f (fromIntegral k) v m)
+insertWith f k v (IntMap m) = IntMap (W.insertWith f (i2w k) v m)
 
 insertWithKey :: (Key -> a -> a -> a) -> Key -> a -> IntMap a -> IntMap a
 insertWithKey f k = insertWith (f k) k
 
 delete :: Key -> IntMap a -> IntMap a
-delete k (IntMap m) = IntMap (W.delete (fromIntegral k) m)
+delete k (IntMap m) = IntMap (W.delete (i2w k) m)
 
 adjust :: (a -> a) -> Key -> IntMap a -> IntMap a
-adjust f k (IntMap m) = IntMap (W.adjust f (fromIntegral k) m)
+adjust f k (IntMap m) = IntMap (W.adjust f (i2w k) m)
 
 adjustWithKey :: (Key -> a -> a) -> Key -> IntMap a -> IntMap a
-adjustWithKey f k (IntMap m) = IntMap (W.adjustWithKey f' (fromIntegral k) m) where
-    f' = f . fromIntegral
+adjustWithKey f k (IntMap m) = IntMap (W.adjustWithKey f' (i2w k) m) where
+    f' = f . w2i
 
 update :: (a -> Maybe a) -> Key -> IntMap a -> IntMap a
-update f k (IntMap m) = IntMap (W.update f (fromIntegral k) m)
+update f k (IntMap m) = IntMap (W.update f (i2w k) m)
 
 updateWithKey :: (Key -> a -> Maybe a) -> Key -> IntMap a -> IntMap a
-updateWithKey f k (IntMap m) = IntMap (W.updateWithKey f' (fromIntegral k) m) where
-    f' = f . fromIntegral
+updateWithKey f k (IntMap m) = IntMap (W.updateWithKey f' (i2w k) m) where
+    f' = f . w2i
 
 union :: IntMap a -> IntMap a -> IntMap a
 union (IntMap m1) (IntMap m2) = IntMap (W.union m1 m2)
@@ -181,7 +183,7 @@ unionWith f (IntMap m1) (IntMap m2) = IntMap (W.unionWith f m1 m2)
 
 unionWithKey :: (Key -> a -> a -> a) -> IntMap a -> IntMap a -> IntMap a
 unionWithKey f (IntMap m1) (IntMap m2) = IntMap (W.unionWithKey f' m1 m2) where
-    f' k = f (fromIntegral k)
+    f' = f . w2i
 
 intersection :: IntMap a -> IntMap b -> IntMap a
 intersection (IntMap m1) (IntMap m2) = IntMap (W.intersection m1 m2)
@@ -191,14 +193,14 @@ intersectionWith f (IntMap m1) (IntMap m2) = IntMap (W.intersectionWith f m1 m2)
 
 intersectionWithKey :: (Key -> a -> b -> c) -> IntMap a -> IntMap b -> IntMap c
 intersectionWithKey f (IntMap m1) (IntMap m2) = IntMap (W.intersectionWithKey f' m1 m2) where
-    f' k = f (fromIntegral k)
+    f' = f . w2i
 
 map :: (a -> b) -> IntMap a -> IntMap b
 map = fmap
 
 mapWithKey :: (Key -> a -> b) -> IntMap a -> IntMap b
 mapWithKey f (IntMap m) = IntMap (W.mapWithKey f' m) where
-    f' = f . fromIntegral
+    f' = f . w2i
 
 foldr :: (a -> b -> b) -> b -> IntMap a -> b
 foldr f z = start
@@ -206,7 +208,7 @@ foldr f z = start
     start (IntMap Empty) = z
     start (IntMap (NonEmpty _ minV Tip)) = f minV z
     start (IntMap (NonEmpty min minV (Bin max maxV l r)))
-        | fromIntegral (xor min max) < (0 :: Int) = goR r (f maxV (f minV (goL l z)))
+        | w2i (xor min max) < 0 = goR r (f maxV (f minV (goL l z)))
         | otherwise = f minV (goL l (goR r (f maxV z)))
     
     goL Tip acc = acc
@@ -221,7 +223,7 @@ foldl f z = start
     start (IntMap Empty) = z
     start (IntMap (NonEmpty _ minV Tip)) = f z minV
     start (IntMap (NonEmpty min minV (Bin max maxV l r)))
-        | fromIntegral (xor min max) < (0 :: Int) = goL (f (f (goR z r) maxV) minV) l
+        | w2i (xor min max) < 0 = goL (f (f (goR z r) maxV) minV) l
         | otherwise = f (goR (goL (f z minV) l) r) maxV
 
     goL acc Tip = acc
@@ -236,7 +238,7 @@ foldrWithKey f z = start
     start (IntMap Empty) = z
     start (IntMap (NonEmpty min minV Tip)) = f' min minV z
     start (IntMap (NonEmpty min minV (Bin max maxV l r)))
-        | fromIntegral (xor min max) < (0 :: Int) = goR r (f' max maxV (f' min minV (goL l z)))
+        | w2i (xor min max) < 0 = goR r (f' max maxV (f' min minV (goL l z)))
         | otherwise = f' min minV (goL l (goR r (f' max maxV z)))
     
     goL Tip acc = acc
@@ -245,7 +247,7 @@ foldrWithKey f z = start
     goR Tip acc = acc
     goR (Bin min minV l r) acc = f' min minV (goL l (goR r acc))
     
-    f' k a b = f (fromIntegral k) a b
+    f' k a b = f (w2i k) a b
 
 foldlWithKey :: (a -> Key -> b -> a) -> a -> IntMap b -> a
 foldlWithKey f z = start
@@ -253,7 +255,7 @@ foldlWithKey f z = start
     start (IntMap Empty) = z
     start (IntMap (NonEmpty min minV Tip)) = f' z min minV
     start (IntMap (NonEmpty min minV (Bin max maxV l r)))
-        | fromIntegral (xor min max) < (0 :: Int) = goL (f' (f' (goR z r) max maxV) min minV) l
+        | w2i (xor min max) < 0 = goL (f' (f' (goR z r) max maxV) min minV) l
         | otherwise = f' (goR (goL (f' z min minV) l) r) max maxV
 
     goL acc Tip = acc
@@ -262,7 +264,7 @@ foldlWithKey f z = start
     goR acc Tip = acc
     goR acc (Bin min minV l r) = goR (goL (f' acc min minV) l) r
     
-    f' a k b = f a (fromIntegral k) b
+    f' a k b = f a (w2i k) b
 
 foldr' :: (a -> b -> b) -> b -> IntMap a -> b
 foldr' f z = start
@@ -270,7 +272,7 @@ foldr' f z = start
     start (IntMap Empty) = z
     start (IntMap (NonEmpty _ minV Tip)) = f minV $! z
     start (IntMap (NonEmpty min minV (Bin max maxV l r)))
-        | fromIntegral (xor min max) < (0 :: Int) = goR r $! f maxV $! f minV $! goL l $! z
+        | w2i (xor min max) < 0 = goR r $! f maxV $! f minV $! goL l $! z
         | otherwise = f minV $! goL l $! goR r $! f maxV $! z
     
     goL Tip acc = acc
@@ -285,7 +287,7 @@ foldl' f z = start
     start (IntMap Empty) = z
     start (IntMap (NonEmpty _ minV Tip)) = f z minV
     start (IntMap (NonEmpty min minV (Bin max maxV l r)))
-        | fromIntegral (xor min max) < (0 :: Int) = s goL (s f (s f (s goR z r) maxV) minV) l
+        | w2i (xor min max) < 0 = s goL (s f (s f (s goR z r) maxV) minV) l
         | otherwise = s f (s goR (s goL (s f z minV) l) r) maxV
 
     goL acc Tip = acc
@@ -302,7 +304,7 @@ foldrWithKey' f z = start
     start (IntMap Empty) = z
     start (IntMap (NonEmpty min minV Tip)) = f' min minV $! z
     start (IntMap (NonEmpty min minV (Bin max maxV l r)))
-        | fromIntegral (xor min max) < (0 :: Int) = goR r $! f' max maxV $! f' min minV $! goL l $! z
+        | w2i (xor min max) < 0 = goR r $! f' max maxV $! f' min minV $! goL l $! z
         | otherwise = f' min minV $! goL l $! goR r $! f' max maxV $! z
     
     goL Tip acc = acc
@@ -311,7 +313,7 @@ foldrWithKey' f z = start
     goR Tip acc = acc
     goR (Bin min minV l r) acc = f' min minV $! goL l $! goR r $! acc
     
-    f' k a b = f (fromIntegral k) a b
+    f' k a b = f (w2i k) a b
 
 foldlWithKey' :: (a -> Key -> b -> a) -> a -> IntMap b -> a
 foldlWithKey' f z = start
@@ -319,7 +321,7 @@ foldlWithKey' f z = start
     start (IntMap Empty) = z
     start (IntMap (NonEmpty min minV Tip)) = s f' z min minV
     start (IntMap (NonEmpty min minV (Bin max maxV l r)))
-        | fromIntegral (xor min max) < (0 :: Int) = s goL (s f' (s f' (s goR z r) max maxV) min minV) l
+        | w2i (xor min max) < 0 = s goL (s f' (s f' (s goR z r) max maxV) min minV) l
         | otherwise = s f' (s goR (s goL (s f' z min minV) l) r) max maxV
 
     goL acc Tip = acc
@@ -328,39 +330,47 @@ foldlWithKey' f z = start
     goR acc Tip = acc
     goR acc (Bin min minV l r) = s goR (s goL (s f' acc min minV) l) r
     
-    f' a k b = f a (fromIntegral k) b
+    f' a k b = f a (w2i k) b
     
     s = ($!)
 
 fromList :: [(Int, a)] -> IntMap a
-fromList = IntMap . W.fromList . fmap (\(k, v) -> (fromIntegral k, v))
+fromList = IntMap . W.fromList . fmap (\(k, v) -> (i2w k, v))
 
 filter :: (a -> Bool) -> IntMap a -> IntMap a
 filter p (IntMap m) = IntMap (W.filter p m)
 
 filterWithKey :: (Key -> a -> Bool) -> IntMap a -> IntMap a
-filterWithKey p (IntMap m) = IntMap (W.filterWithKey (p . fromIntegral) m)
+filterWithKey p (IntMap m) = IntMap (W.filterWithKey (p . w2i) m)
 
 partition :: (a -> Bool) -> IntMap a -> (IntMap a, IntMap a)
 partition p (IntMap m) = let (t, f) = W.partition p m in (IntMap t, IntMap f)
 
 partitionWithKey :: (Key -> a -> Bool) -> IntMap a -> (IntMap a, IntMap a)
-partitionWithKey p (IntMap m) = let (l, r) = W.partitionWithKey (p . fromIntegral) m in (IntMap l, IntMap r)
+partitionWithKey p (IntMap m) = let (l, r) = W.partitionWithKey (p . w2i) m in (IntMap l, IntMap r)
 
 mapMaybe :: (a -> Maybe b) -> IntMap a -> IntMap b
 mapMaybe f (IntMap m) = IntMap (W.mapMaybe f m)
 
 mapMaybeWithKey :: (Key -> a -> Maybe b) -> IntMap a -> IntMap b
-mapMaybeWithKey f (IntMap m) = IntMap (W.mapMaybeWithKey (f . fromIntegral) m)
+mapMaybeWithKey f (IntMap m) = IntMap (W.mapMaybeWithKey (f . w2i) m)
 
 mapEither :: (a -> Either b c) -> IntMap a -> (IntMap b, IntMap c)
 mapEither f (IntMap m) = let (l, r) = W.mapEither f m in (IntMap l, IntMap r)
 
 mapEitherWithKey :: (Key -> a -> Either b c) -> IntMap a -> (IntMap b, IntMap c)
-mapEitherWithKey f (IntMap m) = let (l, r) = W.mapEitherWithKey (f . fromIntegral) m in (IntMap l, IntMap r)
+mapEitherWithKey f (IntMap m) = let (l, r) = W.mapEitherWithKey (f . w2i) m in (IntMap l, IntMap r)
 
 showTree :: Show a => IntMap a -> String
 showTree (IntMap m) = W.showTree m
 
 valid :: IntMap a -> Bool
 valid (IntMap m) = W.valid m
+
+----------------------
+
+i2w :: Int -> Word
+i2w = fromIntegral
+
+w2i :: Word -> Int
+w2i = fromIntegral
